7a68e2977e68b5d9a5f03f7f79d3a0774bd2c7b9
Michele Diegoli <m.diegoli@gmail.com>
2016-05-11 14:59:34 +0200

    Update f3d_dataType_005.js

--- js/f3d_dataType_005.js
+++ js/f3d_dataType_005.js
@@ -50,12 +50,14 @@ var F3D_sketch = {
     		
         },
         fast3d_addCircle: function(name, x, y, r){
-          	var circle = document.createElementNS(NS,"ellipse");
+            var circle = document.createElementNS(NS,"ellipse");
+     	    circle.setAttribute('id'. 'ellipse_'+F3D_Sphere.circle_in_scene);
      	    circle.setAttribute('cx', x);
             circle.setAttribute('cy', y);
             circle.setAttribute('rx', r);
             circle.setAttribute('ry', r);
             circle.setAttribute('class', 'draggable');
+            F3D_Sphere.circle_in_scene++;
             return circle;
             
           },
