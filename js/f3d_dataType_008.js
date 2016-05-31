8574071ac53aeb06d5e94c0488a5743a64e57c77
Michele Diegoli <m.diegoli@gmail.com>
2016-05-31 14:28:41 +0200

    Update f3d_dataType_008.js

--- js/f3d_dataType_008.js
+++ js/f3d_dataType_008.js
@@ -323,13 +323,10 @@ var F3D_Polygon = {
 	  					document.getElementById('svgpaper').appendChild(curve2);
 				*/	
 				var pathlength = path1.length;
-				if(F3D_Scene.curves_group !== ''){
-					F3D_Scene.curves_group.outerHTML = '';
-				}else{
-					F3D_Scene.curves_group = document.createElementNS(NS,"g");
-					F3D_Scene.curves_group.setAttribute('id', 'f3d_curves');
-					document.getElementById('svgpaper').appendChild(F3D_Scene.curves_group);
-				}
+				
+				F3D_Scene.curves_group = document.createElementNS(NS,"g");
+				F3D_Scene.curves_group.setAttribute('id', 'f3d_curves');
+				document.getElementById('svgpaper').appendChild(F3D_Scene.curves_group);
 				for(var i = 0;i<pathlength;i++){
 					var curve1 = document.createElementNS(NS,"ellipse");
 				  	curve1.setAttribute('id', 'f3d_pathellipse_'+i);
