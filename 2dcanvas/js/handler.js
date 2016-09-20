5a37f89f81c6c3e288e7151f7fc8f7b8ca5c031b
Michele Diegoli <m.diegoli@gmail.com>
2016-09-20 18:56:40 +0200

    Update handler.js

--- 2dcanvas/js/handler.js
+++ 2dcanvas/js/handler.js
@@ -5,6 +5,28 @@ var item = {
     old_y: 0,
     drawbool: false,
     selectable: false,
-    draw(){},
-    isSelected(){}
+    startDrawLine: function(canvas,evt){
+        item.old_x = evt.clientX - rect.left;
+        item.old_y = evt.clientY - rect.top;
+        item.drawbool = true;
+        
+    },
+    continueDrawLine:  function(canvas,evt){
+        item.x = evt.clientX - rect.left;
+        item.y = evt.clientY - rect.top;
+        if(item.drawbool){
+          context.beginPath();
+          context.moveTo(item.old_x, item.old_y);
+          context.lineTo(item.x, item.y);
+          context.stroke();
+          item.old_x = item.x;
+          item.old_y = item.y;
+        }
+        
+    },
+    stopDrawLine:  function(canvas,evt){
+        item.drawbool = false;
+        
+    },
+    isSelected: function(){}
 };
