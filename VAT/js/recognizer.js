abcf6fed978b45b96a41e00a2a496579ca139945
Michele Diegoli <m.diegoli@gmail.com>
2016-11-06 15:12:59 +0100

    Create recognizer.js

---
+++ VAT/js/recognizer.js
@@ -0,0 +1,12 @@
+var F2D = {
+  recognizer = function(){
+    var points = [],
+    var segments = paper.path._segments,
+    var maxy, maxx, miny, minx = 0;
+    for (var i = 0, prev, l = segments.length; i < l; i++) {
+        var point = segments[i].point;
+        
+    }
+
+  }
+};
