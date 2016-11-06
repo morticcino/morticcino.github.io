85d95b7b47cd6855451e3598872f3367e5d45b35
Michele Diegoli <m.diegoli@gmail.com>
2016-11-06 15:37:33 +0100

    Update recognizer.js

--- VAT/js/recognizer.js
+++ VAT/js/recognizer.js
@@ -1,12 +1,30 @@
 var F2D = {
+  r: function(){ 
+    return new DollarRecognizer()
+  },
   recognizer: function(segments){
+    
     var points = [];
-    var segments = paper.path._segments;
     var maxy, maxx, miny, minx = 0;
     for (var i = 0, prev, l = segments.length; i < l; i++) {
-        var point = segments[i].point;
-        
+        var point = {x: segments[i].point.x, y: segments[i].points.y};
+        if(maxx < point.x)
+          maxx = point.x;
+        if(minx > point.x)
+          minx = point.x;
+      
+        if(maxy < point.y)
+          maxy = point.y;
+        if(miny > point.y)
+          miny = point.y;
+        points.push(point);
     }
+    var matched = r().Recognize(points);
+	  console.log(matched);  
+	  switch(matched.Name){
+      case "circle":
+        return matched.Name+' '+maxx+' '+minx+' '+maxy+' 'miny;
+        break;
 
   }
 };
