82405000f84fd8659fb9badc9584e58acb087c87
Michele Diegoli <m.diegoli@gmail.com>
2016-11-06 15:18:03 +0100

    Update recognizer.js

--- VAT/js/recognizer.js
+++ VAT/js/recognizer.js
@@ -1,7 +1,7 @@
 var F2D = {
   recognizer = function(segments){
-    var points = [],
-    var segments = paper.path._segments,
+    var points = [];
+    var segments = paper.path._segments;
     var maxy, maxx, miny, minx = 0;
     for (var i = 0, prev, l = segments.length; i < l; i++) {
         var point = segments[i].point;
