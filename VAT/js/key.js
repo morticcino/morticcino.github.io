c16b182902e6f5e59973ab18711c9848ffcd23e6
Michele Diegoli <m.diegoli@gmail.com>
2016-11-08 13:52:40 +0100

    Create key.js

---
+++ VAT/js/key.js
@@ -0,0 +1,19 @@
+function onDocumentKeyDown( event ) {
+
+				switch( event.keyCode ) {
+
+					case 16: isShiftDown = true; break;
+
+				}
+
+			}
+
+			function onDocumentKeyUp( event ) {
+
+				switch ( event.keyCode ) {
+
+					case 16: isShiftDown = false; break;
+
+				}
+
+			}
