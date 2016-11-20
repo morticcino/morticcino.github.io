3d4abd4f2356a60ac777d5b7341509aac69c920c
Michele Diegoli <m.diegoli@gmail.com>
2016-11-20 18:13:11 +0100

    Update draw.js

--- VAT/js/draw.js
+++ VAT/js/draw.js
@@ -338,8 +338,7 @@ function tangent(x1,y1,r1,x2,y2,r2){
 						
 					);
 
-					var linnew THREE.Vector3( poc2x, 0, poc2y ),
-						new THREE.Vector3( poc1x, 0, poc1y )e = new THREE.Line( geometry, material_dx );
+					var line = new THREE.Line( geometry, material_dx );
 					scene.add( line );
 					//group.polygons.appendChild(F3D_Polygon.addPolygon('polygon',poc2x,poc2y,poc1x,poc1y,poc4x,poc4y,poc3x,poc3y));
 					
