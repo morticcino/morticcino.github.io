cdcb903d5a53fa033ac4a00860abcfb760589096
Michele Diegoli <m.diegoli@gmail.com>
2016-11-24 09:15:05 +0100

    Update key.js

--- VAT/js/key.js
+++ VAT/js/key.js
@@ -1,19 +1,31 @@
 function onDocumentKeyDown( event ) {
-
+/*
 				switch( event.keyCode ) {
 
 					case 16: isShiftDown = true; break;
 
 				}
-
-			}
+*/
+	switch( event.key ) {
+		case s: 
+			draw = false;
+			select = true;
+			break;
+		case d: 
+			draw = true;
+			select = false;
+			break;
+		case default:
+			console.log(event.key);
+	}
+}
 
 			function onDocumentKeyUp( event ) {
-
+/*
 				switch ( event.keyCode ) {
 
 					case 16: isShiftDown = false; break;
 
 				}
-
+*/
 			}
