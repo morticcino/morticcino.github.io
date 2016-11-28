a3db8fc67d3a005a787d3c81c96748f03bbcab87
Michele Diegoli <m.diegoli@gmail.com>
2016-11-28 14:50:28 +0100

    Update tools.js

--- VAT/js/tools.js
+++ VAT/js/tools.js
@@ -1,8 +1,23 @@
 function F2DTool(){};
 
-F2DTool.prototype.x = 0;
-F2DTool.prototype.y = 0;
 F2DTool.prototype.registerEvents = function(){};
 F2DTool.prototype.mouseDown = function(){};
 F2DTool.prototype.mouseMove = function(){};
 F2DTool.prototype.mouseUp = function(){};
+
+var F2DDraw = Object.create (F2DTool); 
+
+F2DDraw.prototype.mouseDown = function(e){
+  console.log('F2DDraw mouseDown');
+};
+F2DDraw.prototype.mouseMove = function(){
+  console.log('F2DDraw mouseMove');
+};
+F2DDraw.prototype.mouseUp = function(){
+  console.log('F2DDraw mouseUp');
+};
+f2DDraw.prototype.registerEvents = function(){
+  document.addEventListener('mousedown',this.mouseDown, false);
+  document.addEventListener('mousemove',this.mouseMove, false);
+  document.addEventListener('mouseup',this.mouseUp, false);
+}
