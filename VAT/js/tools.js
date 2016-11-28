e1cc9e30d1edb6ef8eb6e82d868b175630c3e197
Michele Diegoli <m.diegoli@gmail.com>
2016-11-28 14:28:54 +0100

    Create tools.js

---
+++ VAT/js/tools.js
@@ -0,0 +1,8 @@
+function F2DTool(){};
+
+F2DTool.prototype.x = 0;
+F2DTool.prototype.y = 0;
+F2DTool.prototype.registerEvents = function(){};
+F2DTool.prototype.mouseDown = function(){};
+F2DTool.prototype.mouseMove = function(){};
+F2DTool.prototype.mouseUp = function(){};
