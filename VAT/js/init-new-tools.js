ff7bb938056b2fcdff5fa7e941c3515f497edb65
Michele Diegoli <m.diegoli@gmail.com>
2016-11-29 09:44:33 +0100

    Update init-new-tools.js

--- VAT/js/init-new-tools.js
+++ VAT/js/init-new-tools.js
@@ -1,1 +1,14 @@
 F2DDraw.prototype.registerEvents();
+
+document.addEventListener('keydown',function onDocumentKeyDown( event ) {
+	switch( event.key ) {
+		case 's': 
+			F2DSelect.prototype.registerEvents();
+			break;
+		case 'd': 
+			F2DDraw.prototype.registerEvents();
+			break;
+		default:
+			console.log(event.key);
+	}
+},false);
