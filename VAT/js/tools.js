72466db76ad658559dd5f03ac8ca9b1bff1aef9d
Michele Diegoli <m.diegoli@gmail.com>
2016-11-29 14:06:12 +0100

    Update tools.js

--- VAT/js/tools.js
+++ VAT/js/tools.js
@@ -1,6 +1,5 @@
 function F2DTool(){};
 
-F2DTool.prototype.registerEvents = function(){};
 F2DTool.prototype.mouseDown = function(){};
 F2DTool.prototype.mouseMove = function(){};
 F2DTool.prototype.mouseUp = function(){};
