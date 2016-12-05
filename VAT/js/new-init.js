5275af7f25dce4dc990761169df2c5f09990e70a
Michele Diegoli <m.diegoli@gmail.com>
2016-12-05 14:18:28 +0100

    Update new-init.js

--- VAT/js/new-init.js
+++ VAT/js/new-init.js
@@ -6,6 +6,14 @@ function init() {
 	app['container'] = document.createElement( 'div' );
 	document.body.appendChild( app['container'] );
 
+	var info = document.createElement( 'div' );
+	info.style.position = 'absolute';
+	info.style.top = '10px';
+	info.style.width = '100%';
+	info.style.textAlign = 'center';
+	info.innerHTML = '<a href="http://threejs.org" target="_blank">three.js</a> - voxel painter - webgl<br><strong>click</strong>: add voxel, <strong>shift + click</strong>: remove voxel';
+	app['container'].appendChild( info );
+	
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
 	camera.position.set(0, 1000, 0 );
 	camera.lookAt( new THREE.Vector3() );
