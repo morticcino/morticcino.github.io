85407f138e08fb7d28674990df0fde846f42e341
Michele Diegoli <m.diegoli@gmail.com>
2016-12-14 14:23:14 +0100

    Update init-new-tools.js

--- VAT/js/init-new-tools.js
+++ VAT/js/init-new-tools.js
@@ -33,6 +33,17 @@ var sketching = true;
 var objectsToIntersect = {},
     INTERSECTED,SELECTED,intersection = new THREE.Vector3();
 var plane = new THREE.Plane();
+
+var controls = new THREE.TrackballControls( camera );
+controls.rotateSpeed = 1.0;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
+controls.noZoom = false;
+controls.noPan = false;
+controls.staticMoving = true;
+controls.dynamicDampingFactor = 0.3;
+
+
 init();
 render();
 
