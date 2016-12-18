43829d3cb1cece3460264c029a74a8b1ed236713
Michele Diegoli <m.diegoli@gmail.com>
2016-12-18 17:32:27 +0100

    Update f2dinit.js

--- VAT/js/f2dinit.js
+++ VAT/js/f2dinit.js
@@ -68,27 +68,7 @@ NS="http://www.w3.org/2000/svg";
       	}
       }
         
-      function find_index(str){
-      		var res = [];
-      		var re = new RegExp(str+"(\\d+)","g")
-      		if(document.getElementById('svgpaper')){
-      			var match_results = document.getElementById('svgpaper').outerHTML.match(re)
-	      		if(match_results){
-	      			match_results.forEach(function(str){
-		      			res.push(str.match(/(\d+)/g)[1]);
-		      		});	
-	      		}else{
-	      			return '';
-	      		}
-      		}else{
-      			return '';
-      		}
-      			
-      		
-      		
-      		return res;
-      		
-      	}
+      
         
       
       
