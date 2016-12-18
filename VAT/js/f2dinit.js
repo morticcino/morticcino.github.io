ad329716253fcb73ee9f61d5dff8fba5fbae3378
Michele Diegoli <m.diegoli@gmail.com>
2016-12-18 17:29:01 +0100

    Update f2dinit.js

--- VAT/js/f2dinit.js
+++ VAT/js/f2dinit.js
@@ -139,15 +139,7 @@ NS="http://www.w3.org/2000/svg";
 	        	
       }
       
-      function change_group_color(color_value){
-      	if(F3D_Scene.elementToColor !== ''){
-	      	F3D_Scene.elementToColor.setAttribute('style', 'fill:'+color_value+';font-family:bebas;');
-	      	F3D_Scene.colorDict[F3D_Scene.elementToColor.getAttribute('id')]=color_value;	
-	      	save_undo();
-      	}
-      	save_undo();
-      	
-      }
+      
       
       function load(value){
       	//if(value.length > 1){
