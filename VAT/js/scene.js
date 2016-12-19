ad71abbcadb9aa693361733b903d970a393c55a3
Michele Diegoli <m.diegoli@gmail.com>
2016-12-19 14:42:11 +0100

    Update scene.js

--- VAT/js/scene.js
+++ VAT/js/scene.js
@@ -1,36 +1,30 @@
 function load(value){
-      	//if(value.length > 1){
-      		//svg.selectAll("*").remove();
-		var to_load = value;
-		var parser = new DOMParser();
-		var doc = parser.parseFromString(to_load, "image/svg+xml");
-		if(document.getElementById('svgpaper')){
-			document.getElementById("svgpaper").remove();
-			document.getElementById("right_column").appendChild(doc.documentElement);
-			F3D_Scene.sketch_group = document.getElementById('sketch_group');
-			F3D_Scene.hand_draw_objects.length = undo_redo[undo_redo_history].hd;
-			F3D_Scene.extrude_objects.length = undo_redo[undo_redo_history].ex; 
-			F3D_Scene.tentacle_objects.length = undo_redo[undo_redo_history].tn;
-			update_data_structure();
-			svgpaper = document.getElementById("svgpaper");
-			tools(F3D_Polygon.selected_tool);	
-		}
-		
-	//}
-      }
+	var to_load = value;
+	var parser = new DOMParser();
+	var doc = parser.parseFromString(to_load, "image/svg+xml");
+	if(document.getElementById('svgpaper')){
+		document.getElementById("svgpaper").remove();
+		document.getElementById("right_column").appendChild(doc.documentElement);
+		F3D_Scene.sketch_group = document.getElementById('sketch_group');
+		F3D_Scene.hand_draw_objects.length = undo_redo[undo_redo_history].hd;
+		F3D_Scene.extrude_objects.length = undo_redo[undo_redo_history].ex; 
+		F3D_Scene.tentacle_objects.length = undo_redo[undo_redo_history].tn;
+		update_data_structure();
+		svgpaper = document.getElementById("svgpaper");
+		tools(F3D_Polygon.selected_tool);	
+	}
+}
 
 function p_and_z(){
-        	//document.getElementById('svgpaper').setAttribute('viewBox',px+" "+py+" "+sw+" "+sh);
-	        document.getElementById('svgpaper').viewBox.baseVal.x = px;
-		document.getElementById('svgpaper').viewBox.baseVal.y = py;
-		document.getElementById('svgpaper').viewBox.baseVal.width = sw;
- 		document.getElementById('svgpaper').viewBox.baseVal.height = sh;
-        }
+	document.getElementById('svgpaper').viewBox.baseVal.x = px;
+	document.getElementById('svgpaper').viewBox.baseVal.y = py;
+	document.getElementById('svgpaper').viewBox.baseVal.width = sw;
+	document.getElementById('svgpaper').viewBox.baseVal.height = sh;
+}
 
 function save(obj){
-		//return JSON.parse(JSON.stringify(obj));
-		return JSON.stringify(obj);
-	}
+	return JSON.stringify(obj);
+}
 
 function update_data_structure(){
       	var find_results = find_index('f3dtext_group_');
@@ -67,10 +61,10 @@ function update_data_structure(){
 	      		F3D_Scene.tentacle_objects[i].polygons = document.getElementById('f3dtentacle_polygons_group_'+i);
 	      	}
       	}
-      }
+}
       
 function save_undo(){
-		undo_redo_history += 1;
-		undo_redo[undo_redo_history] = {html: document.getElementById('svgpaper').outerHTML, hd: F3D_Scene.hand_draw_objects.length, ex: F3D_Scene.extrude_objects.length,tn: F3D_Scene.tentacle_objects.length};
-		undo_redo.length = (undo_redo_history+1);
-      }
+	undo_redo_history += 1;
+	undo_redo[undo_redo_history] = {html: document.getElementById('svgpaper').outerHTML, hd: F3D_Scene.hand_draw_objects.length, ex: F3D_Scene.extrude_objects.length,tn: F3D_Scene.tentacle_objects.length};
+	undo_redo.length = (undo_redo_history+1);
+}
