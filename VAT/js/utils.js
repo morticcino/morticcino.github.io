719c17f1d6629f8ec04b79585ecf18efc18521ff
Michele Diegoli <m.diegoli@gmail.com>
2016-12-19 14:23:45 +0100

    Update utils.js

--- VAT/js/utils.js
+++ VAT/js/utils.js
@@ -33,3 +33,24 @@ function matrixTransform(event,x,y){
 function json_parse(str){
       	return JSON.parse(str);
       }
+
+
+function m_value(x1,y1,x2,y2){
+	return (y2-y1)/(x2-x1);
+}
+function y_value(m,x,q){
+	return m*x+q;
+}
+function q_value(x1,y1,x2,y2){
+	return -x1*((y2-y1)/(x2-x1))+y1;
+}
+function increaseLength(increment, x1, y1, x2, y2){
+	var newx = x2 + increment/Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2))*(x2-x1);
+	var newy = y2 + increment/Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2))*(y2-y1);
+	return {'x':newx,'y':newy};	
+}
+function increaseLength2(increment, x1, y1, x2, y2){
+	var newx = x2 + increment/Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))*(x1-x2);
+	var newy = y2 + increment/Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))*(y1-y2);
+	return {'x':newx,'y':newy};	
+}
