6998f75e758f48b7172b4380a4c50b3b1ad823db
Michele Diegoli <m.diegoli@gmail.com>
2016-12-27 16:05:04 +0100

    Update circles.js

--- F2D/js/circles.js
+++ F2D/js/circles.js
@@ -2,38 +2,41 @@
 il cerchio viene creato in un gruppo dentro all'svg
 Il gruppo è creato a partire dal broup id
 */
-function circle (x,y) {
-  this.numberOfCircles++;
-  this.svg.getElementById(this.id_group).appendChild('<circle id="cirlce_'+this.numberOfCircles+'" cx="'+x+'" cy="'+y+'" r="1" stroke="black" stroke-width="1" fill="red" />');
-}
+function circle (x,y) {}
 
-circle.prototype.x = 0;
-circle.prototype.y = 0;
-circle.prototype.radius = 1;
-circle.prototype.numberOfCircles = -1;
-circle.prototype.id_group = "group_"+this.numberOfGroups;
-circle.prototype.group = {};
-circle.prototype.object = {};
-circle.prototype.numberOfGroups = 0;
+circle.prototype = {
+  contructor: function(x,y){
+    this.numberOfCircles++;
+    this.svg.getElementById(this.id_group).appendChild('<circle id="cirlce_'+this.numberOfCircles+'" cx="'+x+'" cy="'+y+'" r="1" stroke="black" stroke-width="1" fill="red" />');
+  },
+  x: 0,
+  y: 0,
+  radius: 1,
+  numberOfCircles: -1,
+  numberOfGroups: 0,
+  id_group: "group_"+this.numberOfGroups,
+  group: {},
+  object: {},
 
-circle.prototype.setGroup = function(i){
-  this.numberOfGroups = i;
-}
+  setGroup: function(i){
+    this.numberOfGroups = i;
+  },
 
-circle.prototype.add = function(x,y){
-  this.numberOfCircles++;
-  this.svg.getElementById(this.id_group).appendChild('<circle id="cirlce_'+this.numberOfCircles+'" cx="'+x+'" cy="'+y+'" r="1" stroke="black" stroke-width="1" fill="red" />');
-}
+  add: function(x,y){
+    this.numberOfCircles++;
+    this.svg.getElementById(this.id_group).appendChild('<circle id="cirlce_'+this.numberOfCircles+'" cx="'+x+'" cy="'+y+'" r="'+radius+'" stroke="black" stroke-width="1" fill="red" />');
+  },
 
-circle.prototype.update = function(radius){
+  update: function(radius){
   
-  this.svg.getElementById("cirle_"+this.numberOfCircles).setAttribute('r',radius);
-};
+    this.svg.getElementById("cirle_"+this.numberOfCircles).setAttribute('r',radius);
+  },
 
-circle.prototype.svg = document.getElementById('svgpaper');
-circle.prototype.setCenter = function(new_x,new_y){
-  this.x = new_x;
-  this.y = new_y;
-  this.draw();
-} 
-circle.prototype.setRay = 
+  svg: function(){ return document.getElementById('svgpaper');},
+  setCenter: function(new_x,new_y){
+    this.x = new_x;
+    this.y = new_y;
+    this.draw();
+  } 
+  setRay: function(r){}
+}
