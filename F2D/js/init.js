b1b47fa22a7fa6ec6c31abeab2aae63334ab2e48
Michele Diegoli <m.diegoli@gmail.com>
2016-12-27 16:49:33 +0100

    Update init.js

--- F2D/js/init.js
+++ F2D/js/init.js
@@ -1,5 +1,8 @@
 var app = {};
 app['mouse-events'] = {};
 app['touch-events'] = {};
+app['mouse-down'] = {};
+app['circle_origin_x'] = null;
+app['circle_origin_y'] = null;
 
 var NS="http://www.w3.org/2000/svg";
