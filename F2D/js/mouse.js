429343baf42c4f652cb10e83d21b855cd0eafa18
Michele Diegoli <m.diegoli@gmail.com>
2016-12-27 09:19:52 +0100

    Create mouse.js

---
+++ F2D/js/mouse.js
@@ -0,0 +1,88 @@
+function onWindowResize() {}
+
+function onDocumentMobileMouseMove(event) {
+    var x = event.targetTouches[0].pageX;
+    var y = event.targetTouches[0].pageY;
+    mousemove(x, y);
+}
+
+function onDocumentMouseMove(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    mousemove(x, y);
+}
+
+function mousemove(x, y) {}
+
+function onDocumentMobileMouseDown(event) {
+    var x = event.targetTouches[0].pageX;
+    var y = event.targetTouches[0].pageY;
+    mousedown(x, y);
+}
+
+function onDocumentMouseDown(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    mousedown(x, y);
+}
+
+function mousedown(x, y) {}
+
+function onDocumentMobileMouseUp(event) {
+    mouseup();
+}
+
+function onDocumentMouseUp(event) {
+    mouseup();
+}
+
+function mouseup() {}
+
+
+
+
+function onSelectMobileMouseMove(event) {
+    if (app['mouse_down'] === true) {
+        app['x'] = event.targetTouches[0].pageX;
+        app['y'] = event.targetTouches[0].pageY;
+        selectmove(app['x'], app['y']);
+    }
+}
+
+function onSelectMouseMove(event) {
+    if (app['mouse_down'] === true) {
+        app['x'] = event.clientX;
+        app['y'] = event.clientY;
+        selectmove(app['x'], app['y']);
+    }
+
+}
+
+function selectmove(x, y) {}
+
+
+function onSelectMobileMouseDown(event) {
+    var x = event.targetTouches[0].pageX;
+    var y = event.targetTouches[0].pageY;
+    selectdown(x, y);
+}
+
+function onSelectMouseDown(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    selectdown(x, y);
+}
+
+function selectdown(x, y) {}
+
+
+function onSelectMobileMouseUp(event) {
+    selectup();
+}
+
+function onSelectMouseUp(event) {
+
+    selectup();
+}
+
+function selectup() {}
