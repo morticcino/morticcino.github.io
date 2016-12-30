b3e5830fa2b764f0e7e1be0f2ba1ae6c7f13b95a
Michele Diegoli <m.diegoli@gmail.com>
2016-12-30 14:01:24 +0100

    Update circles.js

--- F2D/js/circles.js
+++ F2D/js/circles.js
@@ -2,41 +2,40 @@
 il cerchio viene creato in un gruppo dentro all'svg
 Il gruppo è creato a partire dal broup id
 */
-function circle(){}
-
-circle.prototype = {
-  contructor: function(x,y){
+function circle(x,y){
+  this.contructor: function(x,y){
     this.numberOfCircles++;
     this.svg.getElementById(this.id_group).appendChild('<circle id="cirlce_'+this.numberOfCircles+'" cx="'+x+'" cy="'+y+'" r="1" stroke="black" stroke-width="1" fill="red" />');
   },
-  x: 0,
-  y: 0,
-  radius: 1,
-  numberOfCircles: -1,
-  numberOfGroups: 0,
-  id_group: "group_"+this.numberOfGroups,
-  group: {},
-  object: {},
+  this.x: 0,
+  this.y: 0,
+  this.radius: 1,
+  this.numberOfCircles: -1,
+  this.numberOfGroups: 0,
+  this.id_group: "group_"+this.numberOfGroups,
+  this.group: {},
+  this.object: {},
 
-  setGroup: function(i){
+  this.setGroup: function(i){
     this.numberOfGroups = i;
   },
 
-  add: function(x,y){
+  this.add: function(x,y){
     this.numberOfCircles++;
     this.svg().getElementById(this.id_group).appendChild('<circle id="cirlce_'+this.numberOfCircles+'" cx="'+x+'" cy="'+y+'" r="'+radius+'" stroke="black" stroke-width="1" fill="red" />');
   },
 
-  update: function(radius){
+  this.update: function(radius){
   
     this.svg().getElementById("cirle_"+this.numberOfCircles).setAttribute('r',radius);
   },
 
-  svg: function(){ return document.getElementById('svgpaper');},
-  setCenter: function(new_x,new_y){
+  this.svg: function(){ return document.getElementById('svgpaper');},
+  this.setCenter: function(new_x,new_y){
     this.x = new_x;
     this.y = new_y;
     this.draw();
   },
-  setRay: function(r){}
+  this.setRay: function(r){}
+
 }
